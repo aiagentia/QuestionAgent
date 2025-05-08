ac4c7e38d2e4171e11862ab61e95776a8cd83ab8
aiagentia <aiagentia@gmail.com>
2025-05-09 00:05:10 +0100

    Update ask.js

--- api/ask.js
+++ api/ask.js
@@ -1,5 +1,3 @@
-// File: pages/api/ask.js
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ success: false, message: 'Method not allowed' });
